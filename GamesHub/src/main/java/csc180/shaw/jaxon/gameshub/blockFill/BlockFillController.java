7bdaf81796e213128c35fd89da2cc8cc996cad25
Talon Shead <tshead@student.neumont.edu>
2026-08-11 09:55:21 -0600

    Added extra buttons and functionality, and started making boards

--- GamesHub/src/main/java/csc180/shaw/jaxon/gameshub/blockFill/BlockFillController.java
+++ GamesHub/src/main/java/csc180/shaw/jaxon/gameshub/blockFill/BlockFillController.java
@@ -1,7 +1,7 @@
 package csc180.shaw.jaxon.gameshub.blockFill;
 
 import javafx.fxml.FXML;
-
+import java.util.Random;
 import java.io.IOException;
 
 import static csc180.shaw.jaxon.gameshub.HelloController.getT;
@@ -17,7 +17,7 @@ public class BlockFillController {
     }
 
     @FXML
-    protected void easyModeButtonClick() {
+    protected void onEasyButtonClick() {
         try {
             changeScene("blockFillViews/block-fill-game-board-easy.fxml", "Block Fill", true);
         } catch (IOException ioe) {
@@ -26,9 +26,18 @@ public class BlockFillController {
     }
 
     @FXML
-    protected void hardModeButtonClick() {
+    protected void onHardButtonClick() {
         try {
             changeScene("blockFillViews/block-fill-game-board-hard.fxml", "Block Fill", true);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void onDifficultySelectClick() {
+        try {
+            changeScene("blockFillViews/block-fill-view.fxml", "Block Fill", false);
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
